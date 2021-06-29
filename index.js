448a8d0d94c9c41536c02b925f3725e3927e89f0
Rungreung <rungreung.sak@gmail.com>
2021-06-29 19:31:39 +0700

    workshop && finnsh course

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-/*
-Loop are used to execute a block of code a number of times or until logical condition is false
-
-ex. 
-for (let i =0; i<10;i++){
-    body
+function appenImageELem(keyword, index){
+    const imgElem = document.createElement('img');
+    imgElem.src = `https://source.unsplash.com/400x225/?${keyword}&sig=${index}`;
+    const galleryElem = document.querySelector('.gallery')
+    galleryElem.appendChild(imgElem);
 }
-} 
-*/ 
-for(let counter = 0; counter <10; counter ++){
-    if (counter %2 !== 0 ){
-        continue; //อะไรไม่เท่ากับ 0 ก็กลับไปทำใหม่  1 3 5 7 9 ไม่เท่ากับ 0 ก็จะไม่ไปทำงานที่ console.log
+function removePhoto(){
+    const galleryElem = document.querySelector('.gallery');
+    galleryElem.innerHTML= ' ';
+}
+
+function searchPhotos(event){
+    const keyword = event.target.value;
+    if (event.key === 'Enter' && keyword !== ""){
+
+        removePhoto();
+        for (let i =1; i<= 9 ; i++){
+            appenImageELem(keyword,i)
+        }
     }
-    console.log(counter)
 
 }
 
-console.log(1%2)
+function run(){
+    const inputElem = document.querySelector('input');
+    inputElem.addEventListener('keydown',searchPhotos)
+}
+
+run();
